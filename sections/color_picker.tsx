b0ef7bd8763a2fba0b45e9828ddd0c7f489f6033
HeyImJoella <joelle.iris@gmail.com>
2026-06-28 16:23:07 +0200

    Color picker added

--- sections/color_picker.tsx
+++ sections/color_picker.tsx
@@ -1,7 +1,25 @@
+"use client";
+
+import { ShuffleIcon, EyedropperIcon } from "@phosphor-icons/react";
+
 export default function ColorPicker() {
   return (
-    <p className="flex flex-1 h-[480px] justify-center bg-red-100 font-mono">
-      ColorPicker
-    </p>
+    <>
+      <div className="flex flex-1 relative h-[480px] justify-center items-center bg-[#C96442] font-mono w-full rounded-2xl">
+        <div className="flex flex-row gap-2 absolute top-6 right-6">
+          <div className="flex align-middle bg-white/10 p-3 rounded-sm">
+            <ShuffleIcon size={20} weight="regular" color="white" />
+          </div>
+
+          <div className="flex align-middle bg-white/10 p-3 rounded-sm">
+            <EyedropperIcon size={20} weight="regular" color="white" />
+          </div>
+        </div>
+
+        <p className="font-mono text-white/40 text-8xl w-full text-center">
+          C96442
+        </p>
+      </div>
+    </>
   );
 }
--- sections/name.tsx
+++ sections/name.tsx
@@ -2,8 +2,8 @@ export default function Name() {
   return (
     <>
       <div className="flex justify-center max-w-7xl w-full mt-16">
-        <div className="flex justify-start w-full py-4 px-6 bg-white rounded-full border border-[#BABABA]">
-          <p className="text-[#BABABA]">Name your color...</p>
+        <div className="flex justify-start w-full py-4 px-6 bg-white rounded-full border border-black/30">
+          <p className="text-black/30">Name your color...</p>
         </div>
       </div>
     </>
