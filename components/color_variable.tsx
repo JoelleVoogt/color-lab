e4933d34d5ba7b81028392e2b6a450dc8af675b0
HeyImJoella <joelle.iris@gmail.com>
2026-06-28 18:17:30 +0200

    Changed to semibold

--- components/color_variable.tsx
+++ components/color_variable.tsx
@@ -13,7 +13,7 @@ export default function ColorVariable({
         className="flex flex-col gap-12 w-full p-4 rounded-lg border border-[#262624]"
         style={{ backgroundColor: `${value}`, color: `${text}` }}
       >
-        <p>{scale}</p>
+        <p className="font-semibold">{scale}</p>
         <p>{value}</p>
       </div>
     </>
