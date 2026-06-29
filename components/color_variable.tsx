6a8a024a8e0d734a6ed8e784621956e3cd41cbdf
HeyImJoella <joelle.iris@gmail.com>
2026-06-29 10:05:39 +0200

    Remove hashtag

--- components/color_variable.tsx
+++ components/color_variable.tsx
@@ -11,7 +11,7 @@ export default function ColorVariable({
     <>
       <div
         className="flex flex-col gap-12 w-full p-4 rounded-lg border border-[#262624]"
-        style={{ backgroundColor: `${value}`, color: `${text}` }}
+        style={{ backgroundColor: `#${value}`, color: `${text}` }}
       >
         <p className="font-semibold">{scale}</p>
         <p>{value}</p>
--- sections/color_output.tsx
+++ sections/color_output.tsx
@@ -3,15 +3,15 @@ import ColorVariable from "@/components/color_variable";
 export default function ColorOutput() {
   return (
     <div className="grid grid-cols-1 lg:grid-cols-9 gap-2 max-w-7xl w-full my-8">
-      <ColorVariable scale="100" value="#F6DDD5" text="black" />
-      <ColorVariable scale="200" value="#ECBCAC" text="black" />
-      <ColorVariable scale="300" value="#E39A82" text="black" />
-      <ColorVariable scale="400" value="#D97757" text="black" />
-      <ColorVariable scale="500" value="#C96442" text="black" />
-      <ColorVariable scale="600" value="#A64626" text="white" />
-      <ColorVariable scale="700" value="#7D341C" text="white" />
-      <ColorVariable scale="800" value="#532313" text="white" />
-      <ColorVariable scale="900" value="#2A1109" text="white" />
+      <ColorVariable scale="100" value="F6DDD5" text="black" />
+      <ColorVariable scale="200" value="ECBCAC" text="black" />
+      <ColorVariable scale="300" value="E39A82" text="black" />
+      <ColorVariable scale="400" value="D97757" text="black" />
+      <ColorVariable scale="500" value="C96442" text="black" />
+      <ColorVariable scale="600" value="A64626" text="white" />
+      <ColorVariable scale="700" value="7D341C" text="white" />
+      <ColorVariable scale="800" value="532313" text="white" />
+      <ColorVariable scale="900" value="2A1109" text="white" />
     </div>
   );
 }
