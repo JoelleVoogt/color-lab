1f07eda5d3b2e0f882489039d653689aeb569dc2
HeyImJoella <joelle.iris@gmail.com>
2026-06-30 09:46:00 +0200

    Optimised spacing

--- sections/color_output.tsx
+++ sections/color_output.tsx
@@ -7,7 +7,7 @@ export default function ColorOutput() {
 
   return (
     <>
-      <div className="grid grid-cols-1 lg:grid-cols-9 gap-2 max-w-7xl w-full my-10">
+      <div className="grid grid-cols-1 lg:grid-cols-9 gap-2 max-w-7xl w-full my-12">
         {scale.map((color) => {
           const contrastWithBlack = wcagContrast(color, "black");
           const contrastWithWhite = wcagContrast(color, "white");
