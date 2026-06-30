89727658af23462d676f36a3b00a8e26a07849a9
HeyImJoella <joelle.iris@gmail.com>
2026-06-30 09:18:01 +0200

    Optimised spacing

--- sections/name.tsx
+++ sections/name.tsx
@@ -1,7 +1,7 @@
 export default function Name() {
   return (
     <>
-      <div className="flex justify-center max-w-7xl w-full mt-12 md:mt-16">
+      <div className="flex justify-center max-w-7xl w-full mt-10">
         <div className="flex justify-start w-full py-4 px-6 bg-[#1A1A1A]/3 dark:bg-[#FAF9F5]/5 rounded-full border  border-[#ceccc2] dark:border-[#e4e0d0]/14">
           <p className="text-black/30 dark:text-white/30">Name your color...</p>
         </div>
